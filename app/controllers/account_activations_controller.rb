254e7731570c898b2dee49544e20339780f25e37
SoKawahara <kawaharasou0216@icloud.com>
2024-10-26 21:14:14 +0900

    Update

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,10 +4,10 @@ class AccountActivationsController < ApplicationController
         if user && !user.activated? && user.authenticated?(:activation ,params[:id])
             user.activate
             log_in user
-            # flash[:success] = "アカウントを有効化しました!"
+            flash[:success] = "アカウントを有効化しました!"
             redirect_to user
         else
-            # flash[:danger] = "アカウントを有効化できませんでした"
+            flash[:danger] = "アカウントを有効化できませんでした"
             redirect_to new_user_path
         end
     end
