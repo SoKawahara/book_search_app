c6db52232d1cd804812cd4942bdf11a755e5a27a
SoKawahara <kawaharasou0216@icloud.com>
2024-10-26 16:57:37 +0900

    Update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./custom/home";
-import "./validates/form";
-import "./toggles/setting-toggle";
-import "./toggles/humburger-toggle";
+import "custom/home";
+import "validates/form";
+import "toggles/setting-toggle";
+import "toggles/humburger-toggle";
