ca0743013c9660745f3dbd91525bd75f85e5c14d
SoKawahara <kawaharasou0216@icloud.com>
2024-11-10 09:14:10 +0900

    Update

--- db/migrate/20241108044509_remove_recommendation_books_confirmation_from_table_name.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRecommendationBooksConfirmationFromTableName < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :users, :recommendation_books_confirmation, :json
-  end
-end
