6d2f3214d2127367790af75a68afe49feb52c559
SoKawahara <kawaharasou0216@icloud.com>
2024-12-19 15:19:25 +0900

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,18 +99,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'https://book-search-app-vdrs.onrender.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :port            => 587,
-    :address         => 'smtp.mailgun.org',
-    :user_name       => ENV['MAILGUN_SMTP_LOGIN'],
-    :password        => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain          => host,
-    :authentication  => :plain
-  }
-
-  
 end
