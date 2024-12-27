f592e8e5b96ea40d007767642a99e3f22f95a6b6
SoKawahara <kawaharasou0216@icloud.com>
2024-12-27 19:36:41 +0900

    Update

--- example.js
+++
@@ -1,6 +0,0 @@
-const showKirin = (entries , obs) => {
-    console.log(entries[0].target);
-}
-
-const kirinObserver = new IntersectionObserver(showKirin);
-kirinObserver.observe(document.querySelector(".hello"));
--- sample.rb
+++
@@ -1,8 +0,0 @@
-module Baseball
-    class Second 
-        def file_with_nesting
-            puts File 
-        end
-    end
-end
-
